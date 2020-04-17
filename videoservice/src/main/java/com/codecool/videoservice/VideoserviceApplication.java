794f49f4a66e6a62c1f18e1af53b610a87724b5c
MelathStofi <melath.stofi@gmail.com>
2020-04-17 12:13:20 +0200

    add eureka server and videoservice

---
+++ videoservice/src/main/java/com/codecool/videoservice/VideoserviceApplication.java
@@ -0,0 +1,46 @@
+package com.codecool.videoservice;
+
+import com.codecool.videoservice.entity.Video;
+import com.codecool.videoservice.repository.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class VideoserviceApplication {
+
+	@Autowired
+	private VideoRepository repo;
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoserviceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner init() {
+		return args -> {
+			Video video1 = Video.builder()
+					.name("Quick-sort with Hungarian (Küküllőmenti legényes) folk dance")
+					.url("https://www.youtube.com/watch?v=ywWBy6J5gz8")
+					.build();
+
+			Video video2 = Video.builder()
+					.name("Shell-sort with Hungarian (Székely) folk dance")
+					.url("https://www.youtube.com/watch?v=CmPA7zE8mx0")
+					.build();
+
+			Video video3 = Video.builder()
+					.name("Gheorghe Zamfir - Frunzulita lemn adus (cantec de nunta)")
+					.url("https://www.youtube.com/watch?v=toBhrnObFzE")
+					.build();
+
+			repo.saveAll(Arrays.asList(video1, video2, video3));
+		};
+
+	}
+
+}
---
+++ videoservice/src/main/java/com/codecool/videoservice/controller/VideoController.java
@@ -0,0 +1,23 @@
+package com.codecool.videoservice.controller;
+
+import com.codecool.videoservice.entity.Video;
+import com.codecool.videoservice.repository.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/videos")
+public class VideoController {
+
+    @Autowired
+    private VideoRepository repo;
+
+    @GetMapping("")
+    public List<Video> getVideos() {
+        return repo.findAll();
+    }
+}
